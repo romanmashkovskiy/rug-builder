2758f6526c08c764c9f7a64ab6d5045708552659
elliotkijo <elliot@kijo.co>
2016-10-19 15:11:49 +0100

    add fonts, delete typography pattern css & js files, update patterns & typography php files

--- wp-content/themes/crucial-trading/page-templates/patterns.php
+++ wp-content/themes/crucial-trading/page-templates/patterns.php
@@ -12,14 +12,22 @@ echo '
 	padding-bottom: 5px;
 }
 </style>';
+echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/assets/dist/master.css">';
 
 $directory = get_template_directory() . '/patterns';
 $scan      = array_values(array_diff(scandir($directory), array('..', '.', 'functions.php')));
 
 for ( $i=0; $i<count($scan); $i++ ) {
 
-	echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/patterns/' . $scan[$i] . '/' . $scan[$i] . '.css">';
-	echo '<script src="' . get_template_directory_uri() . '/patterns/' . $scan[$i] . '/' . $scan[$i] . '.js"></script>';
 	echo '<h1 class="pattern-title">Pattern: ' . ucwords( $scan[$i] ) . '</h1>';
+
+	if ( file_exists( get_template_directory() . '/patterns/' . $scan[$i] . '/' . $scan[$i] . '.css' ) ) {
+		echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/patterns/' . $scan[$i] . '/' . $scan[$i] . '.css">';
+	}
+
+	if ( file_exists( get_template_directory() . '/patterns/' . $scan[$i] . '/' . $scan[$i] . '.js' ) ) {
+		echo '<script src="' . get_template_directory_uri() . '/patterns/' . $scan[$i] . '/' . $scan[$i] . '.js"></script>';
+	}
+	
 	include( get_template_directory() . '/patterns/' . $scan[$i] . '/' . $scan[$i] . '.php' );
 }
--- wp-content/themes/crucial-trading/patterns/typography/typography.php
+++ wp-content/themes/crucial-trading/patterns/typography/typography.php
@@ -12,21 +12,11 @@
 
 ?>
 
-<h1>Heading One</h1>
-<h2>Heading Two</h2>
-<h3>Heading Three</h3>
-<h4>Heading Four</h4>
-<h5>Heading Five</h5>
-<h6>Heading Six</h6>
-<p>This is a paragraph element. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla pretium faucibus. Cras sed porttitor odio, ac eleifend leo. Suspendisse massa turpis, ultricies eget vestibulum ut, viverra eu odio. Nulla consectetur euismod dapibus.</p>
-<ul>
-	<li>Unordered List</li>
-	<li>Unordered List</li>
-	<li>Unordered List</li>
-</ul>
-<ol>
-	<li>Ordered List</li>
-	<li>Ordered List</li>
-	<li>Ordered List</li>
-</ol>
-<a href="#" title="Hyperlink">Hyperlink</a>
+<h1 class="home-banner-header">Serenity</h1>
+<h1 class="standard-banner-header">About Crucial</h1>
+<h1>About Crucial</h1>
+<h2>Login</h2>
+<h2 class="material-header">Coir</h2>
+<h3 class="sub-header">Experience Crucial</h3>
+<a href="#" title="Link">Start Experience</a>
+<p>Use our new 3D rug builder and experience your new creation in all it's glory.</p>
