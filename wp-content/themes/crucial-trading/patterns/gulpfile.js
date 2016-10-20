8c81d98b3fd2a4b33781c6d3f95491130d89d64c
elliotkijo <elliot@kijo.co>
2016-10-20 17:45:20 +0100

    right align right arrow on super slider

--- wp-content/themes/crucial-trading/patterns/gulpfile.js
+++ wp-content/themes/crucial-trading/patterns/gulpfile.js
@@ -1,16 +1,17 @@
-var gulp          = require('gulp'),
+var fs            = require('fs'),
+    gulp          = require('gulp'),
     autoprefixer  = require('gulp-autoprefixer'),
-    sass          = require('gulp-sass'),
-    fs            = require('fs');
+    sass          = require('gulp-sass');
 
 gulp.task('build-patterns-css', function() {
 
-	var files = fs.readdirSync('./');
-	var gulps = [];
+	var files  = fs.readdirSync('./');
+	var gulps  = [];
+	var ignore = [ '.gitignore', 'gulpfile.js', 'node_modules', 'package.json', '.DS_Store' ];
 
 	for ( var i = 0; i < files.length; i++ ) {
 
-		if ( files[i] !== '.gitignore' && files[i] !== 'gulpfile.js' && files[i] !== 'node_modules' && files[i] !== 'package.json' ) {
+		if ( ignore.indexOf(files[i]) === -1 ) {
 
 			var path = './' + files[i] + '/';
 			var dirFiles = fs.readdirSync(path);
