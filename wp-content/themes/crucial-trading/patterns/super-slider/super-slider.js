9d7f68876efe580b12ff57f4008f1afe8ea525f4
elliotkijo <elliot@kijo.co>
2016-10-20 16:36:13 +0100

    make numbers on right of super slider clickable

--- wp-content/themes/crucial-trading/patterns/super-slider/super-slider.js
+++ wp-content/themes/crucial-trading/patterns/super-slider/super-slider.js
@@ -1,8 +1,18 @@
-jQuery(document).ready(function() {
+$ = jQuery;
 
-	jQuery('#super-slider').superslides({
+$(document).ready(function() {
+
+	var slider = $('#super-slider').superslides({
 		play: 10000,
 		animation: 'fade',
 		animation_speed: 1200
+	});
+
+	$('.slide__numbers li').on('click', function() {
+
+		var thisText = $(this).text();
+		var index = parseInt(thisText.replace('0', '').replace('.', '')) - 1;
+
+		$('#super-slider').superslides('animate', index)
 	});
 });
