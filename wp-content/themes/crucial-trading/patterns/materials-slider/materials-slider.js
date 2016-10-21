f036f3be08a871fc06c0dc8f56ec289a54631727
elliotkijo <elliot@kijo.co>
2016-10-21 16:03:56 +0100

    add bxslider and add materials slider php

---
+++ wp-content/themes/crucial-trading/patterns/materials-slider/materials-slider.js
@@ -0,0 +1,6 @@
+var $ = jQuery;
+
+$(document).ready(function() {
+
+	$('.materials-slider').bxSlider();
+});
---
+++ wp-content/themes/crucial-trading/patterns/materials-slider/materials-slider.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Template Name: Super Slider
+ * The large full size slider used on the homepage 
+ *
+ * Contents:
+ *
+ * @package Crucial Trading
+ * @since Crucial Trading 1.0
+ */
+
+function materials_slider() {
+
+	$html = '';
+
+	$args = array(
+		'hide_empty' => false,
+		'orderby'    => 'name',
+	);
+
+	$categories = get_terms( 'product_cat', $args );
+
+	if ( count( $categories ) > 0 ) {
+
+		$all_materials = '';
+
+		for ( $i=0; $i<count( $categories ); $i++ ) {
+
+			$mat_post = get_post( get_term_meta( $categories[$i]->term_id, 'thumbnail_id', true ) );
+			$mat_src  = $mat_post->guid;
+			$mat_alt  = $mat_post->post_title;
+
+			$all_materials .= '<li><img src="' . $mat_src . '" alt="' . $mat_alt . '"></li>';
+		}
+
+		$html .= '<ul class="materials-slider">';
+
+		for ( $i2=0; $i2<count( $categories ); $i2++ ) {
+
+			$cat = $categories[$i2];
+
+			$post = get_post( get_term_meta( $cat->term_id, 'thumbnail_id', true ) );
+			$icon = $post->guid;
+			$alt  = $post->post_title;
+
+			$post_id = $post->ID;
+
+			$title    = ucwords( $alt );
+			$subtitle = $post->description;
+
+			$html .= '<li class="material-slide">';
+
+			$html .= '<h3 class="slide__title">Our Materials</h3>';
+
+			$html .= '<ul class="slide__list">';
+			$html .= $all_materials;
+			$html .= '</ul>';
+
+			$html .= '<div class="slide__content vertical-align">';
+			$html .= '<div class="content__left">';
+			$html .= '<img src="' . $icon . '" alt="' . $alt . '">';
+			$html .= '</div>';
+			$html .= '<div class="content__right">';
+			$html .= '<h3>True Survivor</h3>';
+			$html .= '<h1>' . $title . '</h1>';
+			$html .= '<p>' . $subtitle . '</p>';
+			$html .= '<a href="#">Read More</a>';
+			$html .= '</div>';
+			$html .= '</div>';
+
+			$html .= '</li>';
+		}
+
+		$html .= '</ul>';
+	}
+
+	return $html;
+}
+
+add_shortcode( 'materials-slider', 'materials_slider' );
+
+echo do_shortcode( '[materials-slider]' );
