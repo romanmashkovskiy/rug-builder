14df4852b3e8f9d4d489d84b0510c4042322563a
elliotkijo <elliot@kijo.co>
2016-10-21 17:57:33 +0100

    add header and super slider to home template

--- wp-content/themes/crucial-trading/page-templates/home.php
+++ wp-content/themes/crucial-trading/page-templates/home.php
@@ -8,8 +8,28 @@
  * @since Hogarths 1.0
  */
  
- get_header(); ?> 
- 
- 
+ get_header(); ?>
+
+<?php
+
+// Will set this up properly but just for now will include links here
+
+echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/assets/css/vendor/bootstrap.min.css">';
+echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/assets/css/vendor/animate.min.css">';
+echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/assets/css/dist/master.min.css">';
+echo '<script src="' . get_template_directory_uri() . '/assets/js/vendor/super-slider.min.js"></script>';
+
+include_once(get_template_directory() . '/patterns/header/header.php');
+include_once(get_template_directory() . '/patterns/super-slider/super-slider.php');
+
+echo '<link rel="stylesheet" href="http://localhost:8888/crucial-trading/wp-content/themes/crucial-trading/patterns/header/header.css">';
+echo '<link rel="stylesheet" href="http://localhost:8888/crucial-trading/wp-content/themes/crucial-trading/patterns/super-slider/super-slider.css">';
+echo '<script src="http://localhost:8888/crucial-trading/wp-content/themes/crucial-trading/patterns/super-slider/super-slider.js"></script>';
+
+echo do_shortcode( '[header]' );
+
+echo do_shortcode( '[super-slider]' );
+
+?>
  
  <?php get_footer(); ?>
