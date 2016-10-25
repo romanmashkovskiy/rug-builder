2c7a81041cae2786d2fc1f5ae4685564e4ed1dc4
JT <jord@ten89.co.uk>
2016-10-25 14:12:51 +0100

    Remove comment from JS.

--- wp-content/themes/crucial-trading/patterns/main-menu/main-menu.js
+++ wp-content/themes/crucial-trading/patterns/main-menu/main-menu.js
@@ -4,9 +4,6 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
  */
  /*!
  * classie - class helper functions
