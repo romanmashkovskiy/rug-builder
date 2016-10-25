8e23afbdd26ebc6ded32fdd7944b27a413a0c845
elliotkijo <elliot@kijo.co>
2016-10-25 13:42:39 +0100

    change active icon on  materials slider when next/prev arrows are clicked

--- wp-content/themes/crucial-trading/patterns/materials-slider/materials-slider.js
+++ wp-content/themes/crucial-trading/patterns/materials-slider/materials-slider.js
@@ -8,29 +8,65 @@ $(document).ready(function() {
 
 	$('.next-slide').on('click', function() {
 		slider.goToNextSlide();
+
+		var curActive;
+
+		$('.slide__list li').each(function(i, v) {
+
+			if ( $(v).children('img').hasClass('active') ) {
+				curActive = $(v).data('slide');
+				$(v).children('img').removeClass('active');
+			}
+		});
+
+		var newActive = curActive + 1;
+
+		if ( curActive < 5 ) {
+			$('.slide__list li[data-slide="' + newActive + '"] img').addClass('active');
+		} else {
+			$('.slide__list li[data-slide="0"] img').addClass('active');
+		}
 	});
 
 	$('.prev-slide').on('click', function() {
 		slider.goToPrevSlide();
+
+		var curActive;
+
+		$('.slide__list li').each(function(i, v) {
+
+			if ( $(v).children('img').hasClass('active') ) {
+				curActive = $(v).data('slide');
+				$(v).children('img').removeClass('active');
+			}
+		});
+
+		var newActive = curActive - 1;
+
+		if ( curActive > 0 ) {
+			$('.slide__list li[data-slide="' + newActive + '"] img').addClass('active');
+		} else {
+			$('.slide__list li[data-slide="5"] img').addClass('active');
+		}
 	});
 
 	$('.slide__list li').on('click', function() {
-		var newIndex = $(this).data('slide');
+		var newActive = $(this).data('slide');
 
-		slider.goToSlide(newIndex);
+		slider.goToSlide(newActive);
 
 		$(this).siblings().each(function(i, v) {
 
-			var oldIndex;
+			var curActive;
 
 			if ( $(v).children('img').hasClass('active') ) {
-				oldIndex = $(v).data('slide');
+				curActive = $(v).data('slide');
 			}
 
-			$('li[data-slide="' + oldIndex + '"] img').removeClass('active');
+			$('li[data-slide="' + curActive + '"] img').removeClass('active');
 		})
 
-		$('li[data-slide="' + newIndex + '"] img').addClass('active');
+		$('li[data-slide="' + newActive + '"] img').addClass('active');
 	});
 
 	$('.material-slide .slide__content').css('cssText', 'height: calc(100% - 60px)');
