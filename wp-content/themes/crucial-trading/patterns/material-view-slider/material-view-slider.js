3e6a0763a350a17024f87479b7cb7fc0e8607542
elliotkijo <elliot@kijo.co>
2016-11-01 16:26:56 +0000

    fix material slider prev arrow

--- wp-content/themes/crucial-trading/patterns/material-view-slider/material-view-slider.js
+++ wp-content/themes/crucial-trading/patterns/material-view-slider/material-view-slider.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
 
 	var svgPrev = [
 		'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="31px" height="59px" viewBox="0 0 31 59" version="1.1" class="prev-slide coir">',
-		'<g id="Home" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="square">',
+		'<g id="Home" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="square" stroke="#7C7C7C">',
 		'<g id="vivid" transform="translate(-45.000000, -553.000000)" stroke="#7C7C7C">',
-		'<g id="vivid" transform="translate(-45.000000, -553.000000)" stroke="#7C7C7C">',
-		'<g id="slider-arrow" transform="translate(60.500000, 582.000000) rotate(-180.000000) translate(-60.500000, -582.000000) translate(46.000000, 553.000000)">',
+		'<g id="slider-arrow" transform="translate(60.500000, 582.000000) rotate(-180.000000) translate(-60.500000, -582.000000) translate(46.000000, 553.000000)"  stroke="#7C7C7C">',
 		'<path d="M0.201388889,0.201388889 L28.8071201,28.8071201" id="Line"/>',
 		'<path d="M0,57.6057312 L28.6057312,29" id="Line"/>',
 		'</g>',
@@ -16,9 +15,9 @@ $(document).ready(function() {
 
 	var svgNext = [
 		'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="31px" height="58px" viewBox="0 0 31 58" version="1.1" class="next-slide coir">',
-		'<g id="Home" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="square">',
+		'<g id="Home" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="square" stroke="#7C7C7C">',
 		'<g id="vivid" transform="translate(-1556.000000, -553.000000)" stroke="#7C7C7C">',
-		'<g id="slider-arrow" transform="translate(1557.000000, 553.000000)">',
+		'<g id="slider-arrow" transform="translate(1557.000000, 553.000000)"  stroke="#7C7C7C">',
 		'<path d="M0.201388889,0.201388889 L28.8071201,28.8071201" id="Line"/>',
 		'<path d="M0,57.6057312 L28.6057312,29" id="Line"/>',
 		'</g>',
