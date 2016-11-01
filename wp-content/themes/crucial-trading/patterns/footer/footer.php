cf65fff79c01f9bdaff57a337ea72f104178433e
JT <jord@ten89.co.uk>
2016-11-01 21:05:09 +0000

    Mobile styles. Add decorative line.

--- wp-content/themes/crucial-trading/patterns/footer/footer.php
+++ wp-content/themes/crucial-trading/patterns/footer/footer.php
@@ -24,16 +24,17 @@ function crucial_footer() {
 	// Start building HTML output
 	$html = '';
 	
-	$html .= '<footer> 
+	$html .= '<footer>
+		<div class="footer__line"></div>  
 		<div class="footer-columns clearfix">
-		<div class="footer__col footer__col--social col-md-4">
+		<div class="footer__col footer__col--social col-sm-6 col-md-4">
 			<ul>
 				<li><a href="'.$crucial_social_link_twitter.'" title="Twitter" target="_blank"><i class="icon-crucial-twitter"></i>twitter</a></li>
 				<li><a href="'.$crucial_social_link_insta.'" title="Instagram" target="_blank"><i class="icon-crucial-instagram"></i>instagram</a></li>
 				<li><a href="'.$crucial_social_link_tumblr.'" title="Instagram" target="_blank"><i class="icon-crucial-tumblr"></i>tumblr</a></li>
 			</ul>
 		</div>
-		<div class="footer__col footer__col--nav-1 col-md-4">
+		<div class="footer__col footer__col--nav-1 col-sm-6 col-md-4">
 			<ul>';
 			
 			if (!empty($get_footer_menu_one)) :
@@ -46,7 +47,7 @@ function crucial_footer() {
 			endif;
 			
 		$html .= '</ul></div>
-		<div class="footer__col footer__col--nav-2 col-md-4">
+		<div class="footer__col footer__col--nav-2 hidden-xs hidden-sm col-md-4">
 			<ul>';
 
 			if (!empty($get_footer_menu_two)) :
@@ -62,7 +63,7 @@ function crucial_footer() {
 		$html .= '
 		<div class="footer__info">
 			<span class="footer__info--copyright">&copy; Crucial Trading Ltd '.date("Y").'</span>
-			<span class="footer__info--design-agency"><a href="http://kijo.co" title="Design by KIJO in Birmingham">design by KIJO</a></span>
+			<span class="footer__info--design-agency"><a href="http://kijo.co" title="Designed by KIJO in Birmingham">design by KIJO</a></span>
 		</div>';
 		$html .= '</footer>';
 
