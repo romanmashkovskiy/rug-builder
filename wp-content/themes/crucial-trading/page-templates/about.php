e500b3fd519d355f82b5ed2b23bd5686a36c5215
JT <jord@ten89.co.uk>
2016-11-02 12:33:40 +0000

    Starting the layout for the about-page.

--- wp-content/themes/crucial-trading/page-templates/about.php
+++ wp-content/themes/crucial-trading/page-templates/about.php
@@ -12,12 +12,32 @@ get_header();
 
 echo do_shortcode( '[logo-nav]' );
 
-echo do_shortcode( '[header]' ); ?> 
+//echo do_shortcode( '[header]' ); ?> 
 
 
 
 <div class="about">
-	<div class="about__timeline"></div>
+	
+
+	
+	<div class="about__timeline">
+		<div class="vertical-align">
+			<h3 class="side-title rotate">About Crucial Trading</h3>
+			<h3 class="subtitle"></h3>
+			<h1 class="vertical-align">Our Story</h1>
+			<a href="" title="Our Story">Read More</a>
+		</div>
+	</div>
+	
+	<div class="about__scroller">
+		<div class="vertical-align">
+			<h3 class="subtitle"></h3>
+			<h1 class="vertical-align">Our Process</h1>
+			<a href="" title="Our Process">Read More</a>
+		</div>
+	</div>
+
 </div>
+
 
 <?php get_footer();
