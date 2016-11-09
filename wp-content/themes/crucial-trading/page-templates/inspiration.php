cfc468fc99047b87d3117f0e151000d8ccd37717
elliotkijo <elliot@kijo.co>
2016-11-09 11:26:47 +0000

    hook inspiration page up to feeds

--- wp-content/themes/crucial-trading/page-templates/inspiration.php
+++ wp-content/themes/crucial-trading/page-templates/inspiration.php
@@ -18,6 +18,11 @@ echo do_shortcode( '[share-links]' );
 
 echo '<div class="posts">';
 
+echo do_shortcode( '[fts_twitter twitter_name=crucialtrading tweets_count=2 show_retweets=yes]' );
+
+echo do_shortcode( '[fts_instagram instagram_id=3184970245 pics_count=3 super_gallery=yes image_size=640px icon_size=65px space_between_photos=1px hide_date_likes_comments=yes center_container=no image_stack_animation=no type=user]' );
+
+/*
 echo '<div class="post" id="post-1">';
 echo do_shortcode( '[social-post image="http://d105txpzekqrfa.cloudfront.net/uploads/2016/10/24200959/organic-slide.jpg" title="Our new floor is so beautiful!" time="4 Days Ago" from="twitter"]' );
 echo '</div>';
@@ -37,9 +42,8 @@ echo '</div>';
 echo '<div class="post" id="post-5">';
 echo do_shortcode( '[social-post image="http://d105txpzekqrfa.cloudfront.net/uploads/2016/10/24200959/organic-slide.jpg" from="instagram"]' );
 echo '</div>';
+*/
 
 echo '</div>';
 
 get_footer();
-
-echo do_shortcode( '[fts_twitter twitter_name=crucialtrading tweets_count=1 show_retweets=yes]' );
