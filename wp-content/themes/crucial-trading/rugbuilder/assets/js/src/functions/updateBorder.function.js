900efeedd63d5234a4351693997f807e758484ff
elliotkijo <elliot@kijo.co>
2016-11-11 10:57:53 +0000

    d

---
+++ wp-content/themes/crucial-trading/rugbuilder/assets/js/src/functions/updateBorder.function.js
@@ -0,0 +1,25 @@
+RugBuilder.prototype.updateBorder = function(border) {
+
+	const R = rugBuilder;
+
+	switch (border) {
+
+		case 'Single Border' :
+
+			alert(border)
+
+			break;
+
+		case 'Single & Piping' :
+
+			alert(border)
+
+			break;
+
+		case 'Double Border' :
+
+			alert(border)
+
+			break;
+	}
+}
