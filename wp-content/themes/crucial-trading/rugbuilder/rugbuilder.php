eef60c0b066a4a693dc8e76737868de3c8711a33
elliotkijo <elliot@kijo.co>
2016-11-14 10:10:05 +0000

    get actual border materials when asked for

--- wp-content/themes/crucial-trading/rugbuilder/rugbuilder.php
+++ wp-content/themes/crucial-trading/rugbuilder/rugbuilder.php
@@ -20,7 +20,7 @@ if ( array_key_exists( 'request', $_GET ) ) {
 			$terms = get_terms( array( 'taxonomy' => 'product_cat', 'hide_empty' => false ) );
 
 			for ( $m = 0; $m < count( $terms ); $m++ ) {
-				if ( $terms[$m]->parent == 0 ) {
+				if ( $terms[$m]->parent == 0 && $terms[$m]->slug != 'border-materials' ) {
 					array_push( $res, $terms[$m] );
 				}
 			}
@@ -103,21 +103,13 @@ if ( array_key_exists( 'request', $_GET ) ) {
 
 			/* TO-DO: Implement border cats into WC and get them instead of normal material cats */
 
-			$terms = get_terms( array( 'taxonomy' => 'product_cat', 'hide_empty' => false ) );
+			$parent_term = get_term_by( 'slug', 'border-materials', 'product_cat' );
+			$parent_id   = $parent_term->term_id;
+
+			$terms = get_terms( array( 'taxonomy' => 'product_cat', 'hide_empty' => false, 'parent' => $parent_id ) );
 
 			for ( $m = 0; $m < count( $terms ); $m++ ) {
-				if ( $terms[$m]->parent == 0 ) {
-					array_push( $res, $terms[$m] );
-				}
-			}
-			for ( $m2 = 0; $m2 < count( $res ); $m2++ ) {
-
-				$material_id = $res[$m2]->term_id;
-
-				$thumb_id = get_woocommerce_term_meta( $material_id, 'thumbnail_id', true );
-				$thumb    = wp_get_attachment_url( $thumb_id );
-
-				$res[$m2]->thumb = $thumb;
+				array_push( $res, $terms[$m] );
 			}
 
 			break;
