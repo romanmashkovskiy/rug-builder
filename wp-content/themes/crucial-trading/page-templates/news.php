189457fdcdd04caf30552eb865cd96008a5e4793
JT <jord@ten89.co.uk>
2016-11-15 18:01:30 +0000

    Small changes to header. Created news patterns.

---
+++ wp-content/themes/crucial-trading/page-templates/news.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: News
+ *
+ * The news home page for displaying posts
+ *
+ * @package Hogarths
+ * @since Hogarths 1.0
+ */
+ 
+get_header();
+
+echo do_shortcode( '[logo-nav]' );
+
+echo do_shortcode( '[header size="small"]' );
+
+echo do_shortcode( '[news-categories]' );
+
+
+
+get_footer();
