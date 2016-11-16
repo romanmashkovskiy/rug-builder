1d221b8509bd7a11aa940d2e605208dc3647d123
JT <jord@ten89.co.uk>
2016-11-16 14:28:09 +0000

    Continue work on news page layout.

--- wp-content/themes/crucial-trading/patterns/news-posts/news-posts.php
+++ wp-content/themes/crucial-trading/patterns/news-posts/news-posts.php
@@ -17,15 +17,35 @@
 	
 	$news_posts = new WP_Query( array( 'post_type' => 'post' ) );
 	
-	$html .= '<div class="news-posts box-shadow">';
+	$html .= '<div class="news-posts">';
 		 
 		// The Loop 
 		while ( $news_posts->have_posts() ) : $news_posts->the_post();
 			
-			$title = get_the_title();
+			// Get post data
+			$news_title = get_the_title();
+			$news_date = get_the_date();
+			$news_cat = '';
+			$news_excerpt = get_the_excerpt();
 			
-			$html .= $title;
-	
+			// Build article card
+			$html .= '<article class="news__item" id="'.get_the_ID().'">
+					<div class="news__item__category">'.$news_cat.'</div>
+					<div class="news__item__box">
+						<div class="news__item__contact-wrap">
+							<div class="news__item__heading"><h3>'.$news_title.'</h3></div>
+							<div class="news__item__date">'.$news_date.'</div>
+							<div class="news__item__excerpt">'.$news_excerpt.'</div>
+							<div class="news__item__link"><a href="'.get_the_permalink().'" title="'.$news_title.'">'.$news_title.'</a></div>
+						</div>
+						<div class="news__item__image-wrap">
+							<div class="news__item__image">
+								$news-image 
+							</div>
+						</div>
+					</div>
+				</article>';
+
 		endwhile;
 
 	$html .= '</div>';
