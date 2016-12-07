c6611b6b9b4f4afffa8cbc247a8ffeb93afd3718
elliotkijo <elliot@kijo.co>
2016-12-07 16:24:20 +0000

    add shop page template

---
+++ wp-content/themes/crucial-trading/page-templates/shop.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Shop
+ *
+ * The shop page template
+ *
+ * @package Crucial Trading
+ * @since Crucial Trading 1.0
+ */
+
+echo 1;
+
+header( 'Location: ' . site_url() . '/materials' );
