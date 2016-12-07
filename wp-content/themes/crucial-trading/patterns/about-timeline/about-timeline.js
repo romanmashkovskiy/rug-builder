f5d25f9539e67d27999b74fac5fe8af1b997b2f7
elliotkijo <elliot@kijo.co>
2016-12-07 17:49:15 +0000

    make several mobile css fixes

--- wp-content/themes/crucial-trading/patterns/about-timeline/about-timeline.js
+++ wp-content/themes/crucial-trading/patterns/about-timeline/about-timeline.js
@@ -40,12 +40,12 @@ var $ = jQuery;
 	    
 	  });
 	});
-
+/*
  var s = skrollr.init({
       render: function(data) {
           //Debugging - Log the current scroll position.
           console.log(data.curTop);
       }
   });
-
+*/
 });
