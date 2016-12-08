b5deb8d088d7c1c21b96330a608157e4748c7d28
elliotkijo <elliot@kijo.co>
2016-12-08 18:59:26 +0000

    changes to inspiration page - add icon, makes text bigger, add shadow behind text

--- wp-content/themes/crucial-trading/assets/js/src/script.js
+++ wp-content/themes/crucial-trading/assets/js/src/script.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
 
 	$('#user_login').attr('placeholder', 'Username or Email');
 	$('#user_pass').attr('placeholder', 'Password');
+
+	if ( $('body').hasClass('page-template-inspiration') ) {
+
+		$('.fts-twitter-link-image').each(function(i, e) {
+			$(e).append('<i class="icon-crucial-instagram"></i>')
+		});
+
+		$('.fts-instagram-wrapper').each(function(i, e) {
+			$(e).append('<i class="icon-crucial-instagram"></i>')
+		});
+
+		$('.fts-twitter-text').each(function(i, e) {
+			$(e).append('<div class="twitter-shadow"></div>')
+		});
+	}
 });
