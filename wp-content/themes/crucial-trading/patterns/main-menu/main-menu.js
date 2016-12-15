7e8dce839e829bd24c7128f5ef8a4fc95a0cbd33
elliotkijo <elliot@kijo.co>
2016-12-15 12:47:31 +0000

    fix js error

--- wp-content/themes/crucial-trading/patterns/main-menu/main-menu.js
+++ wp-content/themes/crucial-trading/patterns/main-menu/main-menu.js
@@ -34,16 +34,3 @@ var $ = jQuery;
 	    event.stopPropagation();
 	});
 });
-
-async function read () {
-  var html = await getRandomPonyFooArticle();
-  var md = hget(html, {
-    markdown: true,
-    root: 'main',
-    ignore: '.at-subscribe,.mm-comments,.de-sidebar'
-  });
-  var txt = marked(md, {
-    renderer: new Term()
-  });
-  console.log(txt);
-}
