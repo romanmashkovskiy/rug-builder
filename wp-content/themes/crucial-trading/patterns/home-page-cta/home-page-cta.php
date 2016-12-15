52e11e258e4c2ecde26d5bf6e36ac40c03d44bf2
elliotkijo <elliot@kijo.co>
2016-12-15 13:06:50 +0000

    update home page links

--- wp-content/themes/crucial-trading/patterns/home-page-cta/home-page-cta.php
+++ wp-content/themes/crucial-trading/patterns/home-page-cta/home-page-cta.php
@@ -34,7 +34,7 @@ function home_page_cta( $atts = '' ) {
 			$bg_col = '#383838';
 			$src    = get_template_directory_uri() . '/assets/icons/pin.svg';
 			$link   = site_url() . '/find-retailer';
-			$attrs  = 'class="no-styling"';
+			$attrs  = 'class="no-effect"';
 		}
 		else if ( $atts['id'] == 2 ) {
 
@@ -59,7 +59,7 @@ function home_page_cta( $atts = '' ) {
 			$bg_col = '#343233';
 			$src    = get_template_directory_uri() . '/assets/icons/brochure.svg';
 			$link   = '#';
-			$attrs  = 'class="order-brochure no-styling" data-product-name="Our brochure" data-product-id="' . $brochure_id . '"';
+			$attrs  = 'class="order-brochure no-effect" data-product-name="Our brochure" data-product-id="' . $brochure_id . '"';
 		}
 
 		$html .= '<div class="home-page-cta" style="background-color:' . $bg_col . ';">';
