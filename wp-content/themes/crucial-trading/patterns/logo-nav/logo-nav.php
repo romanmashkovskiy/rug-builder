835e0209a859d8a27283f5945608b8a0f34141c3
elliotkijo <elliot@kijo.co>
2017-01-25 17:50:05 +0000

    upload black logo on home page

--- wp-content/themes/crucial-trading/patterns/logo-nav/logo-nav.php
+++ wp-content/themes/crucial-trading/patterns/logo-nav/logo-nav.php
@@ -20,8 +20,15 @@ function logo_nav_shortcode( $atts = '' ) {
 
 	$html .= '<div class="top-bar__left vertical-align">';
 	$html .= '<a href="'.site_url().'">';
-	$html .= '<img src="http://d105txpzekqrfa.cloudfront.net/uploads/2016/10/26174004/logo.svg" alt="Crucial Trading - where inspiration begins" class="full-logo">';
-	$html .= '<img src="http://d105txpzekqrfa.cloudfront.net/uploads/2016/10/26174002/logo-mobile.svg" alt="Crucial Trading - where inspiration begins" class="mobi-logo">';
+
+	if ( get_page_template_slug() == 'page-templates/home.php' ) {
+		$html .= '<img src="http://d105txpzekqrfa.cloudfront.net/uploads/20170125174752/logo-black.svg" alt="Crucial Trading - where inspiration begins" class="full-logo">';
+		$html .= '<img src="http://d105txpzekqrfa.cloudfront.net/uploads/20170125174756/logo-mobile-black.svg" alt="Crucial Trading - where inspiration begins" class="mobi-logo">';
+	} else {
+		$html .= '<img src="http://d105txpzekqrfa.cloudfront.net/uploads/2016/10/26174004/logo.svg" alt="Crucial Trading - where inspiration begins" class="full-logo">';
+		$html .= '<img src="http://d105txpzekqrfa.cloudfront.net/uploads/2016/10/26174002/logo-mobile.svg" alt="Crucial Trading - where inspiration begins" class="mobi-logo">';
+	}
+
 	$html .= '</a>';
 	$html .= '</div>';
 
