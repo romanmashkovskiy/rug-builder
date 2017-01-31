d8b15ef7832aede32c3f085f378095464a807413
elliotkijo <elliot@kijo.co>
2017-01-31 12:08:34 +0000

    start wc thankyou and update rgbldr border and disable max zoom

---
+++ wp-content/themes/crucial-trading/woocommerce/checkout/thankyou.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Thankyou page
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/thankyou.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( $order ) : ?>
+
+	<p class="woocommerce-thankyou-order-received">
+		<?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); ?>
+	</p>
+
+	<ul class="woocommerce-thankyou-order-details order_details">
+		<li class="order">
+			<?php _e( 'Order Number:', 'woocommerce' ); ?>
+			<strong><?php echo $order->get_order_number(); ?></strong>
+		</li>
+		<li class="date">
+			<?php _e( 'Date:', 'woocommerce' ); ?>
+			<strong><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></strong>
+		</li>
+	</ul>
+
+	<div class="clear"></div>
+
+	<?php do_action( 'woocommerce_thankyou', $order->id ); ?>
+
+<?php else : ?>
+
+	<p class="woocommerce-thankyou-order-received">
+		<?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), null ); ?>
+	</p>
+
+<?php endif; ?>
