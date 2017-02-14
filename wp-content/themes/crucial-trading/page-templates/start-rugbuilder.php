aa86d74ded0254a38f23bf70b3ec4de2ced3f7f3
elliotkijo <elliot@kijo.co>
2017-02-14 17:50:19 +0000

    adjust start rugbuilder page styling

--- wp-content/themes/crucial-trading/page-templates/start-rugbuilder.php
+++ wp-content/themes/crucial-trading/page-templates/start-rugbuilder.php
@@ -16,14 +16,16 @@ echo do_shortcode( '[logo-nav]' );
 ?>
 
 <div id="hello">
-	<img src="http://d105txpzekqrfa.cloudfront.net/uploads/20161213142159/rug-builder-white-icon.svg" class="logo" alt="Rug Builder">
-	<div class="hello__content">
-		<h3>Build Your Own</h3>
-		<h1>Rug Builder</h1>
-		<p>With thousands of combination, our easy-to-use Rug Builder puts you in the shoes of the designer.</p>
-		<a href="<?php echo site_url(); ?>/rugbuilder" class="start">Start Building</a>
+	<div class="vertical-align">
+		<img src="http://d105txpzekqrfa.cloudfront.net/uploads/20161213142159/rug-builder-white-icon.svg" class="logo" alt="Rug Builder">
+		<div class="hello__content">
+			<h3>Build Your Own</h3>
+			<h1>Rug Builder</h1>
+			<p>With thousands of combination, our easy-to-use Rug Builder puts you in the shoes of the designer.</p>
+			<a href="<?php echo site_url(); ?>/rugbuilder" class="start">Start Building</a>
+		</div>
+		<h3 class="name">Rug Builder</h3>
 	</div>
-	<h3 class="name">Rug Builder</h3>
 </div>
 
 <?php
