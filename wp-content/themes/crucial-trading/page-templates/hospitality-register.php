38e329b8f719912083409026a879142cc92208d8
elliotkijo <elliot@kijo.co>
2017-02-15 14:29:29 +0000

    redirect to builder from register for hosp users

--- wp-content/themes/crucial-trading/page-templates/hospitality-register.php
+++ wp-content/themes/crucial-trading/page-templates/hospitality-register.php
@@ -8,6 +8,19 @@
  * @since Crucial Trading 1.0
  */
 
+if ( is_user_logged_in() ) {
+
+	$user    = wp_get_current_user();
+	$roles   = $user->roles;
+
+	foreach ( $roles as $role ) {
+		if ( $role == 'hospitality' ) {
+			header( 'Location: ' . site_url() . '/hospitality-builder' );
+			break;
+		}
+	}
+}
+
 get_header();
 
 echo do_shortcode( '[header size="small"]' );
