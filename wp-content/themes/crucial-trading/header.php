58d7bb9c6c69f480df29b630470ae645a22c7726
JT <jord@ten89.co.uk>
2017-05-10 16:26:45 +0100

    [deploy: production] Add Google webmasters verif code

--- wp-content/themes/crucial-trading/header.php
+++ wp-content/themes/crucial-trading/header.php
@@ -19,6 +19,7 @@ $header_class = WC()->cart->get_cart_contents_count() == 0 ? 'basket-empty' : 'b
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="google-site-verification" content="frbcypQV4caMUho2FkpvOasdks8mHtic44EeduDBdqM" />
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<?php include get_template_directory() . '/header-partials/modernizr.php'; ?>
 		<?php wp_head(); ?>
