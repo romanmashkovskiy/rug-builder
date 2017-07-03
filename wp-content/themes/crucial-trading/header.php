43df799f147bfc1ee23185ad1c3cbea9425f30bd
JT <jord@ten89.co.uk>
2017-07-03 11:29:38 +0100

    [deploy: Production] Remove verify from header and put in directory

--- wp-content/themes/crucial-trading/header.php
+++ wp-content/themes/crucial-trading/header.php
@@ -20,7 +20,6 @@ $header_class = WC()->cart->get_cart_contents_count() == 0 ? 'basket-empty' : 'b
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="google-site-verification" content="frbcypQV4caMUho2FkpvOasdks8mHtic44EeduDBdqM" />
-		<meta name="_globalsign-domain-verification" content="57qHSGeZO8oe3hcmeV09RxntVM73uFCy3Crs0HQgFe" />
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<?php include get_template_directory() . '/header-partials/modernizr.php'; ?>
 		<?php wp_head(); ?>
