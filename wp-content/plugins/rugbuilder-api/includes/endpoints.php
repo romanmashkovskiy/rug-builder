18f181d2ec37baad40810bea6814b549fe8b17fd
Connor Moore <connor@kijo.co>
2017-07-17 17:00:09 +0100

    removed footer from email

--- wp-content/plugins/rugbuilder-api/includes/endpoints.php
+++ wp-content/plugins/rugbuilder-api/includes/endpoints.php
@@ -383,7 +383,7 @@
           arial=""
           sans-serif=""
         >
-    			<table border="0" cellpadding="0" cellspacing="0" height="150px" width="100%">
+    			<table border="0" cellpadding="0" cellspacing="0" height="100px" width="100%">
             <tbody>
               <tr>
                 <td align="center" valign="top" style="font-family: Open Sans, sans-serif;">
@@ -499,31 +499,7 @@
     </table>
     <!-- End Body -->
 
-    			<!-- Footer -->
-    		<table border="0" cellpadding="10" cellspacing="0" id="template_footer" style="width: 100%;">
-          <tbody>
-            <tr>
-          <td valign="top"
-            style="font-family: Open Sans, sans-serif; padding: 0; -webkit-border-radius: 6px;">
-    				<table border="0" cellpadding="10" cellspacing="0" width="100%">
-              <tbody>
-                <tr>
-                  <td colspan="2"
-                    valign="middle"
-                    id="credit"
-                    style="font-family: Arial; padding: 0 48px 48px 48px; -webkit-border-radius: 6px;
-                      border: 0; color: #888888; font-size: 12px; line-height: 125%; text-align: center;"
-                  >
-    							  <p style="font-family: Open Sans, sans-serif;">Copyright Crucial Trading Ltd</p>
-    							</td>
-    						</tr>
-              </tbody>
-            </table>
-          </td>
-    		</tr>
-      </tbody>
-    </table>
-    <!-- End Footer -->
+    
     </body>';
 
     return $template;
