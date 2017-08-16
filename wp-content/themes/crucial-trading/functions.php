a8a3cb2c48f79b37e9fe29a24ae9ee5d8291bb47
JT <jord@ten89.co.uk>
2017-08-16 17:09:16 +0100

    Move scripts

--- wp-content/themes/crucial-trading/functions.php
+++ wp-content/themes/crucial-trading/functions.php
@@ -164,20 +164,31 @@ add_action( 'widgets_init', 'crucial_trading_widgets_init' );
  * Enqueue scripts and styles.
  */
 function crucial_trading_scripts() {
-	wp_enqueue_style( 'crucial-trading-style', get_stylesheet_uri() );
+	//wp_enqueue_style( 'crucial-trading-style', get_stylesheet_uri() );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	wp_enqueue_style( 'master', get_template_directory_uri() . '/assets/css/dist/master.min.css', false );
+	wp_enqueue_style( 'master', get_template_directory_uri() . '/assets/css/dist/master.min.css', true );
 
-	wp_enqueue_script( 'gmap', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC2TBY8zzbNJ9o0DeoLZGZM6oDCDOruKdE', false );
-	wp_enqueue_script( 'master', get_template_directory_uri() . '/assets/js/dist/master.min.js', false );
+	wp_enqueue_script( 'master', get_template_directory_uri() . '/assets/js/dist/master.min.js', '', '', true );
 
 	// https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap
 }
 add_action( 'wp_enqueue_scripts', 'crucial_trading_scripts' );
+
+
+/**
+ * Load Google Maps on retailer page only
+ */
+
+ function ct_enqueue_gmaps() {
+   if ( is_page( 'find-retailer' ) ) {
+     wp_enqueue_script( 'gmap', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC2TBY8zzbNJ9o0DeoLZGZM6oDCDOruKdE', '', '', true );
+   }
+ }
+ add_action( 'wp_enqueue_scripts', 'ct_enqueue_gmaps' );
 
 
 /**
