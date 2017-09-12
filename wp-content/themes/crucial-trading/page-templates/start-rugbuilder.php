f83fc39f605c44477446dc5219faabe51ad85dd0
JT <jord@ten89.co.uk>
2017-09-12 16:16:53 +0100

    Fix spelling mistake start RB page

--- wp-content/themes/crucial-trading/page-templates/start-rugbuilder.php
+++ wp-content/themes/crucial-trading/page-templates/start-rugbuilder.php
@@ -15,18 +15,22 @@ echo do_shortcode( '[logo-nav]' );
 
 ?>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 <div id="hello">
 	<div class="vertical-align">
 		<img src="https://d105txpzekqrfa.cloudfront.net/uploads/20161213142159/rug-builder-white-icon.svg" class="logo" alt="Rug Builder">
 		<div class="hello__content">
-			<h3>Build Your Own</h3>
+			<h3><?php echo get_post_meta(get_the_ID(), 'subtitle', true); ?></h3>
 			<h1>Rug Builder</h1>
-			<p>With thousands of combination, our easy-to-use Rug Builder puts you in the shoes of the designer.</p>
+			<p><?php the_content(); ?></p>
 			<a href="<?php echo site_url(); ?>/rugbuilder" class="start">Start Building</a>
 		</div>
-		<h3 class="name">Rug Builder</h3>
+		<h3 class="name"><?php echo get_post_meta(get_the_ID(), 'subtitle', true); ?></h3>
 	</div>
 </div>
+
+<?php endwhile; endif; ?>
 
 <?php
 
