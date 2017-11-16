0bdb43d6ebfe8a2a88b28f27c1e2a1d280a0bb57
Daveyon Mayne <daveyon@kijo.co>
2017-11-16 15:34:28 +0000

    Added extra studio accordion

--- wp-content/themes/crucial-trading/patterns/retailer-result-dropdown/retailer-result-dropdown.php
+++ wp-content/themes/crucial-trading/patterns/retailer-result-dropdown/retailer-result-dropdown.php
@@ -8,7 +8,7 @@
  * @since Crucial Trading 1.0
  */
 
- function show_room_retailers($main_header = '', $dist = '', $terms = 'showroom', $online = false ) {
+ function retailers($main_header = '', $dist = '', $terms = 'showroom', $online = false ) {
    $showroom_args = array(
    	'post_type' => 'retailer',
    	'orderby'   => 'menu_order',
@@ -33,6 +33,52 @@
        //$iterator =   ( (int)$iterator_ );
 
        $loop .= retailer_loop($title, $post_id, $dist, '', $online);
+     }
+
+   endif;
+
+   return <<<HTML
+
+   <div class="retailer-result-dropdown panel-group">
+     <div class="retailer-result-dropdown__header">
+       <h2>$main_header</h2>
+     </div>
+     $loop
+   </div>
+
+
+HTML;
+ }
+
+ // TODO: Refactor later as this duplication is wrong!!
+ function studio_retailers($main_header = '', $dist = '', $terms = 'studio', $online = false, $uk_retailers ) {
+   $showroom_args = array(
+     'post_type' => 'retailer',
+     'orderby'   => 'menu_order',
+     'order'     => 'ASC',
+     'tax_query' => array(
+       array(
+         'taxonomy' => 'retailer_type',
+         'field'    => 'slug',
+         'terms'    => $terms,
+       ),
+     ),
+   );
+
+   $showroom_query = new WP_Query( $showroom_args );
+   $loop = '';
+   if ( $showroom_query->have_posts() ) :
+     $row = 0;
+     foreach ($showroom_query->posts as $show_post) {
+       $post_id = $show_post->ID;
+       $title = $show_post->post_title;
+       $dist = round( $show_post->distance );
+       $iterator_ = $show_post->i;
+       //$iterator =   ( (int)$iterator_ );
+       $dist = $uk_retailers[$row]->distance;
+    
+       $loop .= retailer_loop($title, $post_id, $dist, '', $online);
+       ++$row;
      }
 
    endif;
