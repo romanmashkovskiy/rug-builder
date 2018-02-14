dca54a56e6e26a71903a7d45ab045cd763669697
JT <jord@ten89.co.uk>
2018-02-14 11:11:27 +0000

    Disable password change notifications

--- wp-content/themes/crucial-trading/functions.php
+++ wp-content/themes/crucial-trading/functions.php
@@ -89,8 +89,10 @@ function crucial_trading_setup() {
 	 */
 	add_filter( 'send_email_change_email', '__return_false' );
 
-	if ( !function_exists( 'wp_password_change_notification' ) ) {
-    function wp_password_change_notification() {}
+	if ( ! function_exists( 'wp_password_change_notification' ) ) {
+	    function wp_password_change_notification( $user ) {
+	        return;
+	    }
 	}
 
 	// This theme uses wp_nav_menu() in one location.
