a097b8dfb069472a7dae54f530e8e9c9a9b17c68
Connor Moore <connor@kijo.co>
2018-03-23 10:02:43 +0000

    Body text to lowercase for tool tip modal box

--- wp-content/themes/crucial-trading/rugbuilder-hospitality/assets/js/src/Tour.js
+++ wp-content/themes/crucial-trading/rugbuilder-hospitality/assets/js/src/Tour.js
@@ -36,7 +36,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
       element: "#drawer",
       title: "STRUCTURES",
-      content: "USE THIS PANEL TO CHOOSE YOUR STRUCTURE",
+      content: "Use this panel to choose your structure",
       placement: "right",
 
       onShow: function (key, value) {
@@ -49,7 +49,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
       element: "#progressMenuLower",
       title: "PROGRESS BAR",
-      content: "USE THE PROGRESS BAR TO BUILD YOUR DESIGN",
+      content: "Use the progress bar to build your design",
       placement: "bottom",
 
       onShow: function (key, value) {
@@ -61,7 +61,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#progressMenuLink1",
     title: "COLOURS",
-    content: "CLICK A COLOUR TO CHANGE YOUR DESIGN",
+    content: "Click a colour to change your design",
     placement: "bottom",
     backdropPadding: 20,
 
@@ -74,7 +74,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#progressMenuLink1",
     title: "HOVER EFFECT",
-    content: "HOVER OVER YOUR COLOUR TO SEE WHAT PART OF THE DESIGN IT EFFECTS",
+    content: "Hover over your colour to see what part of the design it effects",
     placement: "bottom",
     backdropPadding: 20,
 
@@ -87,7 +87,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#drawer",
     title: "CHOOSE A COLOUR",
-    content: "CHOOSE A COLOUR FROM THE DRAWER TO ADD IT TO YOUR DESIGN",
+    content: "Choose a colour from the drawer to add it to your design",
     placement: "right",
 
     onShow: function (key, value) {
@@ -99,7 +99,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#progressMenuLower",
     title: "CHANGE YOUR DESIGN",
-    content: "TO CHANGE YOUR DESIGN CLICK ON A COLOUR OR THE STRUCTURE IN THE PROGRESS BAR",
+    content: "To change your design click on a colour or the structure in the progress bar",
     placement: "bottom",
 
     onShow: function (key, value) {
@@ -111,7 +111,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#hosp_builder_choices",
     title: "THE SIDE BAR",
-    content: "ALL OF the PARTS YOU HAVE SELECTED WILL BE LISTED IN THE SIDE BAR",
+    content: "All of the parts you have selected will be listed in the side bar",
     placement: "left",
     backdropPadding: 10,
   })
@@ -120,7 +120,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#restartBuild",
     title: "START AGAIN",
-    content: "IF YOU WISH TO START YOUR DESIGN AGAIN THEN JUST CLICK START AGAIN",
+    content: "If you wish to start your design again then just click start agaiN",
     placement: "bottom",
     backdropPadding: 10,
   })
@@ -129,7 +129,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#finishDesignButton",
     title: "DESIGN COMPLETE",
-    content: 'CLICK "FINISH YOUR DESIGN" TO PRINT OR EMAIL YOUR SELECTIONS',
+    content: 'Click "finish your design" to print or email your selections',
     placement: "top",
     backdropPadding: 10,
 
@@ -143,7 +143,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#summaryButtons",
     title: "PRINT OR EMAIL",
-    content: "PRINT OR EMAIL TO SAVE YOUR DETAILS OR SEND TO THE MANUFACTURER",
+    content: "Print or email to save your details or send to the manufacturer",
     placement: "top",
     backdropPadding: 10,
 
@@ -156,7 +156,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#summaryViewEdit",
     title: "GOT A BETTER IDEA?",
-    content: "CLICK EDIT TO MAKE EDITS TO YOUR DESIGN",
+    content: "Click edit to make edits to your design",
     placement: "top",
     backdropPadding: 10,
   })
@@ -165,7 +165,7 @@ RugBuilder.prototype.Tour = function() {
   tour.addStep({
     element: "#exitBuilder",
     title: "FINISHED",
-    content: "WHEN YOU'RE FINISHED JUST HIT THE EXIT BUTTON TO RETURN TO THE CRUCIAL TRADING WEBSITE",
+    content: "When you're finished just hit the exit button to return to the crucial trading website",
     placement: "bottom",
     backdropPadding: 10,
   })
