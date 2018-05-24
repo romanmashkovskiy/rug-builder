bd611d43aca306c5e14178f23498a8a646b34725
JT <jord@ten89.co.uk>
2018-05-24 18:01:48 +0100

    Add opt-ins to checkout

--- wp-content/themes/crucial-trading/inc/woocommerce/remove-billing-fields.php
+++ wp-content/themes/crucial-trading/inc/woocommerce/remove-billing-fields.php
@@ -79,5 +79,45 @@ function order_fields($fields) {
 }
 
 /*********************************************************
- * Change Shipping to Delivery
- *********************************************************/
+* GDPR & Privacy opt-ins
+*********************************************************/
+
+add_action( 'woocommerce_review_order_before_submit', 'crucial_add_checkout_privacy_policy', 9 );
+
+ function crucial_add_checkout_privacy_policy() {
+
+   woocommerce_form_field( 'privacy_policy', array(
+       'type'          => 'checkbox',
+       'class'         => array('form-row privacy'),
+       'label_class'   => array('woocommerce-form__label woocommerce-form__label-for-checkbox checkbox'),
+       'input_class'   => array('woocommerce-form__input woocommerce-form__input-checkbox input-checkbox'),
+       'required'      => true,
+       'label'         => 'I agree to my personal data being stored and used to process this enquiry <a href="/privacy-policy">View Privacy Policy</a> Please tick here to opt-in.',
+   ));
+
+}
+
+add_action( 'woocommerce_review_order_before_submit', 'crucial_add_checkout_marketing_consent', 9 );
+
+ function crucial_add_checkout_marketing_consent() {
+
+   woocommerce_form_field( 'marketing_consent', array(
+       'type'          => 'checkbox',
+       'class'         => array('form-row privacy'),
+       'label_class'   => array('woocommerce-form__label woocommerce-form__label-for-checkbox checkbox'),
+       'input_class'   => array('woocommerce-form__input woocommerce-form__input-checkbox input-checkbox'),
+       'required'      => false,
+       'label'         => 'I would like to receive marketing communication including promotions, special offers, news and events from Crucial Trading <a href="/privacy-policy">View Privacy Policy</a> Please tick here to opt in.',
+   ));
+
+}
+
+// Show notice if customer does not tick
+
+add_action( 'woocommerce_checkout_process', 'bbloomer_not_approved_privacy' );
+
+ function bbloomer_not_approved_privacy() {
+     if ( ! (int) isset( $_POST['privacy_policy'] ) ) {
+         wc_add_notice( __( 'Please acknowledge the Privacy Policy' ), 'error' );
+     }
+}
