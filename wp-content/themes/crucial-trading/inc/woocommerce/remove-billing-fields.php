f0bda99d14e0366278e32ae9672c22a50ce3dbef
JT <jord@ten89.co.uk>
2018-05-24 19:07:12 +0100

    show opt-in in WC order backend

--- wp-content/themes/crucial-trading/inc/woocommerce/remove-billing-fields.php
+++ wp-content/themes/crucial-trading/inc/woocommerce/remove-billing-fields.php
@@ -92,7 +92,7 @@ add_action( 'woocommerce_review_order_before_submit', 'crucial_add_checkout_priv
        'label_class'   => array('woocommerce-form__label woocommerce-form__label-for-checkbox checkbox'),
        'input_class'   => array('woocommerce-form__input woocommerce-form__input-checkbox input-checkbox'),
        'required'      => true,
-       'label'         => 'I agree to my personal data being stored and used to process this enquiry <a href="/privacy-policy">View Privacy Policy</a> Please tick here to opt-in.',
+       'label'         => 'I agree to my personal data being stored and used to process this order. This includes sharing my personal information and the details of my order with our partner retailers. Read our <a href="/privacy-policy">Privacy Policy</a> for more information. Please tick here to opt-in.',
    ));
 
 }
@@ -107,17 +107,58 @@ add_action( 'woocommerce_review_order_before_submit', 'crucial_add_checkout_mark
        'label_class'   => array('woocommerce-form__label woocommerce-form__label-for-checkbox checkbox'),
        'input_class'   => array('woocommerce-form__input woocommerce-form__input-checkbox input-checkbox'),
        'required'      => false,
-       'label'         => 'I would like to receive marketing communication including promotions, special offers, news and events from Crucial Trading <a href="/privacy-policy">View Privacy Policy</a> Please tick here to opt in.',
+       'label'         => 'I would like to receive marketing communication including promotions, special offers, news and events from Crucial Trading. <a href="/privacy-policy">View Privacy Policy</a> Please tick here to opt in.',
    ));
 
 }
 
-// Show notice if customer does not tick
+// Show notice if customer does not tick required
 
 add_action( 'woocommerce_checkout_process', 'bbloomer_not_approved_privacy' );
 
  function bbloomer_not_approved_privacy() {
      if ( ! (int) isset( $_POST['privacy_policy'] ) ) {
-         wc_add_notice( __( 'Please acknowledge the Privacy Policy' ), 'error' );
+         wc_add_notice( __( 'We need your consent to our Privacy Policy to complete the checkout' ), 'error' );
      }
+}
+
+
+/**
+ * Update the order meta with field value
+ */
+
+add_action( 'woocommerce_checkout_update_order_meta', 'ct_privacy_field_display_admin_order_meta' );
+
+function ct_privacy_field_display_admin_order_meta( $order_id ) {
+    if ( ! empty( $_POST['privacy_policy'] ) ) {
+        update_post_meta( $order_id, 'privacy_policy', sanitize_text_field( $_POST['privacy_policy'] ) );
+        update_post_meta( $order_id, 'marketing_consent', sanitize_text_field( $_POST['marketing_consent'] ) );
+    }
+}
+
+
+/**
+ * Display field value on the order edit page
+ */
+add_action( 'woocommerce_admin_order_data_after_billing_address', 'ct_custom_checkout_field_display_admin_order_meta', 10, 1 );
+
+function ct_custom_checkout_field_display_admin_order_meta($order){
+
+    $privacy_policy_consent_meta = get_post_meta( $order->id, 'privacy_policy', true );
+    $marketing_policy_consent_meta = get_post_meta( $order->id, 'marketing_consent', true );
+
+    if ($privacy_policy_consent_meta === '1') :
+      $privacy_policy_consent = 'Yes';
+    else :
+      $privacy_policy_consent = 'No';
+    endif;
+
+    if ($marketing_policy_consent_meta === '1') :
+      $marketing_policy_consent = 'Yes';
+    else :
+      $marketing_policy_consent = 'No';
+    endif;
+
+    echo '<p><strong>'.__('Privacy Consent').':</strong> <br/>' . $privacy_policy_consent . '</p>';
+    echo '<p><strong>'.__('Marketing Consent').':</strong> <br/>' . $marketing_policy_consent . '</p>';
 }
